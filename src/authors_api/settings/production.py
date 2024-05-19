b92f21d1edb1b9841839865a1f16bbd0a9175341
Mārcis <marcisreb@gmail.com>
2024-05-19 20:02:41 +0300

    first commit, setup

---
+++ src/authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env
+
+ADMINS = [("Mārcis Upenieks", "api.imperfect@gmail.com")]
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
