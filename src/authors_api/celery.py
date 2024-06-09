509ddf51efebc9e3a8b74c0e28b264a801c85d52
Mārcis <marcisreb@gmail.com>
2024-06-09 06:59:10 +0300

    feat: add jwt authentication and other auth routes

--- src/authors_api/celery.py
+++ src/authors_api/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 # TODO: change this in production
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authors_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authors_api.settings.local')
 
 app = Celery('authors_api')
 
